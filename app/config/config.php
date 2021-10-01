66c07f32e09824feb20202d856b7217a4ba571b9
Zaka Noor <zakanoor@outlook.co.id>
2021-10-02 02:08:56 +0700

    add config database for hosting

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,7 @@
 <?php
     $basepath = "http://localhost/santree/";
 
+    // $mysqli = new mysqli("localhost","id17678214_root","IbxHWFZ]x[T1KCSh","id17678214_santree_db");
     $mysqli = new mysqli("localhost","root","whoami","santree_db");
     
     if($mysqli->connect_errno) {
